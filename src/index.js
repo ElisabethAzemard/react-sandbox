9f7fb2557a70a3e6283e130e01a9c8f8fc2ec3ac
Elisabeth Azémard <elie.azemard@gmail.com>
2019-10-30 11:12:56 +0100

    Create first component.

--- src/index.js
+++ src/index.js
@@ -4,20 +4,20 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-const tick = () => {
+// const tick = () => {
 
-  const element = (
-    <div>
-      <h1>Hello, world!</h1>
-      <p>{ new Date().toLocaleTimeString() }</p>
-    </div>
-  )
-  ReactDOM.render(element, document.getElementById('root'));
-}
+//   const element = (
+//     <div>
+//       <h1>Hello, world!</h1>
+//       <p>{ new Date().toLocaleTimeString() }</p>
+//     </div>
+//   )
+//   ReactDOM.render(element, document.getElementById('root'));
+// }
 
-setInterval(tick, 1000);
+// setInterval(tick, 1000);
 
-//ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
