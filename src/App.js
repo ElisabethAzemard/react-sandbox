00cecb7145f237a3f28f89208992b7e07e07060e
Elisabeth Azémard <elie.azemard@gmail.com>
2019-10-30 11:26:45 +0100

    Create component to add two numbers.

--- src/App.js
+++ src/App.js
@@ -8,10 +8,24 @@ function Bienvenue(props) {
   )
 }
 
+// créer un composant fonctionnel qui admet 2 propriétés
+// a & b qui affiche "la somme de a et b = a + b"
+
+const Add = (props) => {
+  let { a, b } = props;
+
+  return (
+    <p>La somme de {props.a} et {props.b} = {props.a + props.b} </p>
+  );
+};
+
 function App() {
   return (
     <div>
       <Bienvenue nom="Elisabeth"/>
+      <Add a={5} b={8}/>
+      <Add a={2} b={1229}/>
+      <Add a={483} b={1928}/>
     </div>
   );
 }
